361053e5990bc0fdcd3452635e8f3b8a0c372988
aminaouini <aminaouini565@gmail.com>
2020-01-04 18:08:06 +0100

    a

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,34 +2,120 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import List from './components/List.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
+    this.state = {
+      name: "",
+      id: "",
+      bedOrSuit: "",
+      fromDate: "",
+      toDate: ""
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.nameChange = this.nameChange.bind(this);
+    this.idChange = this.idChange.bind(this);
+    this.bedOrSuitChange = this.bedOrSuitChange.bind(this);
+    this.fromDateChange = this.fromDateChange.bind(this);
+    this.toDateChange = this.toDateChange.bind(this);
   }
 
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.post('/insert', this.state)
+      .then((res) => {
+        console.log(res.config.data);
+
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
   }
 
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+  nameChange(event) {
+    event.preventDefault();
+
+    this.setState({ name: event.target.value })
+
+  }
+
+  idChange(event) {
+    event.preventDefault();
+
+    this.setState({ id: event.target.value })
+
+  }
+  bedOrSuitChange(event) {
+    event.preventDefault();
+
+    this.setState({ bedOrSuit: event.target.value })
+
+
+  }
+  fromDateChange(event) {
+    event.preventDefault();
+
+    this.setState({ fromDate: event.target.value })
+
+
+  }
+  toDateChange(event) {
+    event.preventDefault();
+
+    this.setState({ toDate: event.target.value })
+
+
+  }
+
+
+  render() {
+    return (
+      <div id="aouiniHotel">
+        <h1>Aouini Hotel</h1>
+        <div id="HotelData">
+          {/* <h1>Item List</h1>
+      <List items={this.state.items} /> */}
+          <div id="insert">
+            <form onSubmit={this.handleSubmit}>
+              <h2>insert your reservation</h2>
+              <hr></hr>
+              <div className="input">
+                <label form="name">Full Name:* </label>
+                <input type="text" id="fullName" value={this.state.value} onChange={this.nameChange}></input>
+              </div>
+              <br></br>
+              <div className="input">
+                <label form="id">ID:* </label>
+                <input type="text" id="id" value={this.state.value} onChange={this.idChange}></input>
+              </div>
+              <br></br>
+              <div className="input">
+                <label form="Nbeds">bed/suit:* </label>
+                <input type="text" id="bedNumbers" name="bedOrSuit" value={this.state.value} onChange={this.bedOrSuitChange}></input>
+              </div>
+              <br></br>
+              <div className="input">
+                <label form="date">from date:* </label>
+                <input type="text" id="fromDate" name="from" value={this.state.value} onChange={this.fromDateChange}></input>
+                <br></br>
+              </div>
+              <br></br>
+              <div className="input">
+                <label form="date">to date:* </label>
+                <input type="text" id="toDate" name="to" value={this.state.value} onChange={this.toDateChange}></input>
+              </div>
+              <br></br>
+              <button type="submite" id="subm">submite</button>
+            </form>
+          </div>
+
+        </div>
+      </div>
+
+    )
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,29 +1,62 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var assert = require('assert');
+var mongoose = require('mongoose');
+
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
-// var items = require('../database-mongo');
+var items = require('../database-mongo');
 
 var app = express();
-
+var url = 'mongodb://localhost:3000/data';
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
-
+app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(bodyParser.json());
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
-
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+app.get('/', (err) => {
+  // console.log(err);
+  res.send('index')
+    .catch((err) => {
+      console.log(err);
+      throw err
+    })
 });
 
-app.listen(3000, function() {
+app.post('/insert', function (req, res) {
+  // items.selectAll(function (err, data) {
+  //   if (err) {
+  //     res.sendStatus(500);
+  //   } else {
+  //     res.json(data);
+  //   }
+  // });
+
+  var data = {
+    fullName: req.body.name,
+    id: req.body.id,
+    roomOrSuit: req.body.bedOrSuit,
+    from: req.body.from,
+    to: req.body.to
+  }
+  mongoose.connect(url, { useNewUrlParser: true });
+
+  mongoose.connect(url, function (err, db) {
+    assert.equal(null, err);
+    db.coolection('client-reservation').insertOne(data, function (err, result) {
+      assert.equal(null, err);
+      console.log('item inserted');
+
+
+    })
+  })
+
+  res.redirect('/');
+});
+
+app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
 
